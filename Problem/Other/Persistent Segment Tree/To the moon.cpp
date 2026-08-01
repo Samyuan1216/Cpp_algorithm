0c7a6f2052aab43eae85e31e3c4eb88e0a895a4f
samyuan <zsy3013922392@outlook.com>
2026-08-01 11:59:56 +0800

    练习

---
+++ Problem/Other/Persistent Segment Tree/To the moon.cpp
@@ -0,0 +1,439 @@
+// https://www.spoj.com/problems/TTM/
+
+#include <bits/extc++.h>
+namespace ranges = std::ranges;
+
+using i64 = long long;
+
+#ifndef YUAN_DEBUG
+struct __X
+{
+    __X& operator<<(const auto& str) {return *this;}
+    void sp([[maybe_unused]] const std::string& str = "") {}
+} dout;
+#define debug(x)
+#endif
+
+template<class Info, class Tag>
+struct Seg_Tree
+{
+private:
+    int n, cnt = 0;
+    std::vector<Info> info;
+    std::vector<Tag> tag;
+    std::vector<bool> has_tag;
+    std::vector<int> root, left, right;
+
+    int new_node()
+    {
+        info.push_back({});
+        tag.push_back({});
+        has_tag.push_back({});
+        left.push_back(-1);
+        right.push_back(-1);
+
+        return cnt++;
+    }
+
+    int clone(int o)
+    {
+        int rt = new_node();
+        info[rt] = info[o];
+        tag[rt] = tag[o];
+        has_tag[rt] = has_tag[o];
+        left[rt] = left[o];
+        right[rt] = right[o];
+
+        return rt;
+    }
+
+    void up(int i)
+    {
+        info[i] = info[left[i]] + info[right[i]];
+    }
+
+    void apply(int i, const Tag &t)
+    {
+        bool success = info[i].apply(t); 
+        assert(success);
+
+        if (has_tag[i])
+        {
+            tag[i].apply(t);
+        }
+        else
+        {
+            tag[i] = t;
+            has_tag[i] = true;
+        }
+    }
+
+    void down(int i)
+    {
+        if (has_tag[i])
+        {
+            left[i] = clone(left[i]), right[i] = clone(right[i]);
+
+            apply(left[i], tag[i]);
+            apply(right[i], tag[i]);
+            has_tag[i] = false;
+        }
+    }
+
+    int build(int l, int r)
+    {
+        int rt = new_node();
+        if (l != r)
+        {
+            int mid = std::midpoint(l, r);
+            left[rt] = build(l, mid);
+            right[rt] = build(mid + 1, r);
+
+            up(rt);
+        }
+
+        return rt;
+    }
+
+    int build(int l, int r, const std::vector<Info> &init_arr)
+    {
+        int rt = new_node();
+        if (l == r)
+        {
+            info[rt] = init_arr[l];
+        }
+        else
+        {
+            int mid = std::midpoint(l, r);
+            left[rt] = build(l, mid, init_arr);
+            right[rt] = build(mid + 1, r, init_arr);
+
+            up(rt);
+        }
+
+        return rt;
+    }
+
+    int modify(int i, int l, int r, int ql, int qr, const Tag &t)
+    {
+        int rt = clone(i);
+        if (ql <= l && r <= qr)
+        {
+            if (info[rt].apply(t))
+            {
+                if (has_tag[rt])
+                {
+                    tag[rt].apply(t);
+                }
+                else
+                {
+                    tag[rt] = t;
+                    has_tag[rt] = true;
+                }
+
+                return rt;
+            }
+        }
+
+        int mid = std::midpoint(l, r);
+        down(rt);
+
+        if (ql <= mid)
+        {
+            left[rt] = modify(left[rt], l, mid, ql, qr, t);
+        }
+
+        if (qr > mid)
+        {
+            right[rt] = modify(right[rt], mid + 1, r, ql, qr, t);
+        }
+
+        up(rt);
+        return rt;
+    }
+
+    Info query(int i, int l, int r, int ql, int qr)
+    {
+        if (ql <= l && r <= qr)
+        {
+             return info[i];
+        }
+
+        int mid = std::midpoint(l, r);
+        down(i);
+
+        if (qr <= mid)
+        {
+            return query(left[i], l, mid, ql, qr);
+        }
+
+        if (ql > mid)
+        {
+            return query(right[i], mid + 1, r, ql, qr);
+        }
+
+        return query(left[i], l, mid, ql, qr) + query(right[i], mid + 1, r, ql, qr);
+    }
+
+    template<class F>
+    std::optional<int> find_first(int i, int l, int r, int ql, int qr, F check)
+    {
+        if (ql <= l && r <= qr)
+        {
+            if (!check(info[i]))
+            {
+                return std::nullopt;
+            }
+
+            if (l == r)
+            {
+                return l;
+            }
+        }
+
+        int mid = std::midpoint(l, r);
+        down(i);
+
+        std::optional<int> res;
+        if (ql <= mid)
+        {
+            res = find_first(left[i], l, mid, ql, qr, check);
+        }
+
+        if (!res && qr > mid)
+        {
+            res = find_first(right[i], mid + 1, r, ql, qr, check);
+        }
+
+        return res;
+    }
+
+    template<class F>
+    std::optional<int> find_last(int i, int l, int r, int ql, int qr, F check)
+    {
+        if (ql <= l && r <= qr)
+        {
+            if (!check(info[i]))
+            {
+                return std::nullopt;
+            }
+
+            if (l == r)
+            {
+                return l;
+            }
+        }
+
+        int mid = std::midpoint(l, r);
+        down(i);
+
+        std::optional<int> res;
+        if (qr > mid)
+        {
+            res = find_last(right[i], mid + 1, r, ql, qr, check);
+        }
+
+        if (!res && ql <= mid)
+        {
+            res = find_last(left[i], l, mid, ql, qr, check);
+        }
+
+        return res;
+    }
+
+    template<class F>
+    std::optional<int> find_kth(int i, int l, int r, long long k, F get_cnt)
+    {
+        if (l == r)
+        {
+            return l;
+        }
+
+        int mid = std::midpoint(l, r);
+        down(i);
+
+        long long left_cnt = get_cnt(info[left[i]]);
+        if (k <= left_cnt)
+        {
+            return find_kth(left[i], l, mid, k, get_cnt);
+        }
+        else
+        {
+            return find_kth(right[i], mid + 1, r, k - left_cnt, get_cnt);
+        }
+    }
+
+public:
+    Seg_Tree(int n, int m) : n(n), root(m + 1)
+    {
+        if (n <= 0)
+        {
+            return;
+        }
+
+        info.reserve(4 * n + 80 * m);
+        tag.reserve(4 * n + 80 * m);
+        has_tag.reserve(4 * n + 80 * m);
+        left.reserve(4 * n + 80 * m);
+        right.reserve(4 * n + 80 * m);
+
+        root[0] = build(0, n - 1);
+    }
+
+    Seg_Tree(const std::vector<Info>& init_arr, int m) : n(init_arr.size()), root(m + 1)
+    {
+        if (n <= 0)
+        {
+            return;
+        }
+
+        info.reserve(4 * n + 80 * m);
+        tag.reserve(4 * n + 80 * m);
+        has_tag.reserve(4 * n + 80 * m);
+        left.reserve(4 * n + 80 * m);
+        right.reserve(4 * n + 80 * m);
+
+        root[0] = build(0, n - 1, init_arr);
+    }
+    
+    void modify(int l, int r, int u, int v, const Tag &t)
+    {
+        if (l > r || l < 0 || r >= n)
+        {
+            return;
+        }
+
+        root[u] = modify(root[v], 0, n - 1, l, r, t);
+    }
+
+    Info query(int l, int r, int v)
+    {
+        if (l > r || l < 0 || r >= n)
+        {
+            return Info();
+        }
+
+        return query(root[v], 0, n - 1, l, r);
+    }
+
+    template<class F> 
+    std::optional<int> find_first(int l, int r, int v, F check)
+    {
+        if (l > r || l < 0 || r >= n)
+        {
+            return std::nullopt;
+        }
+
+        return find_first(root[v], 0, n - 1, l, r, check);
+    }
+
+    template<class F> 
+    std::optional<int> find_last(int l, int r, int v, F check)
+    {
+        if (l > r || l < 0 || r >= n)
+        {
+            return std::nullopt;
+        }
+
+        return find_last(root[v], 0, n - 1, l, r, check);
+    }
+
+    template<class F>
+    std::optional<int> find_kth(long long k, int v, F get_cnt)
+    {
+        if (n == 0 || get_cnt(info[root[v]]) < k)
+        {
+            return std::nullopt;
+        }
+
+        return find_kth(root[v], 0, n - 1, k, get_cnt);
+    }
+};
+
+void solve()
+{
+    int n, m;
+    std::cin >> n >> m;
+
+    struct Tag
+    {
+        i64 add = 0;
+
+        void apply(const Tag &t)
+        {
+            add += t.add;
+        }
+    };
+
+    struct Info
+    {
+        i64 sum = 0, len = 1;
+
+        Info operator+(const Info &other) const
+        {
+            return {sum + other.sum, len + other.len};
+        }
+
+        bool apply(const Tag &t)
+        {
+            sum += t.add * len;
+            return true;
+        }
+    };
+
+    std::vector<Info> init(n);
+    for (int i = 0, x; i < n; ++i)
+    {
+        std::cin >> x;
+
+        init[i] = {x, 1};
+    }
+
+    Seg_Tree<Info, Tag> tr(init, m);
+    for (int i = 0, t = 0; i < m; ++i)
+    {
+        char op;
+        std::cin >> op;
+
+        if (op == 'C')
+        {
+            int l, r, d;
+            std::cin >> l >> r >> d;
+            --l, --r;
+
+            ++t;
+            tr.modify(l, r, t, t - 1, {d});
+        }
+        else if (op == 'Q')
+        {
+            int l, r;
+            std::cin >> l >> r;
+            --l, --r;
+
+            std::cout << tr.query(l, r, t).sum << "\n";
+        }
+        else if (op == 'H')
+        {
+            int l, r, cur;
+            std::cin >> l >> r >> cur;
+            --l, --r;
+
+            std::cout << tr.query(l, r, cur).sum << "\n";
+        }
+        else
+        {
+            std::cin >> t;
+        }
+    }
+}
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    int t = 1;
+    while (t--)
+    {
+        solve();
+    }
+}
